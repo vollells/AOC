2389be6eed50ebf04df81d85258793dd132fa182
vollells <vicle728@student.liu.se>
2020-12-01 13:54:19 +0100

    day1 a&b done

--- 2020/1/day_1.py
+++ 2020/1/day_1.py
@@ -1,7 +1,18 @@
-def day1():
+def day1_a():
 
     numbers = {int(x) for x in open("input.txt", "r")}
 
     for num in numbers:
-        if (2020 - num) in numbers: 
+        if (2020 - num) in numbers:
                 print(num * (2020 - num))
+                return
+
+def day1_b():
+
+    numbers = {int(x) for x in open("input.txt", "r")}
+
+    for num1 in numbers:
+        for num2 in numbers:
+            if (2020 - num1 - num2) in numbers:
+                print(num1* num2 * (2020 - num1 - num2))
+                return
