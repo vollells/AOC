c0436a3f5d554263d3329f7d63a2ce5ce8efc997
Vollells <vicle728@student.liu.se>
2023-11-13 21:25:52 +0100

    day9 done

--- 2022/9/day_9.cc
+++ 2022/9/day_9.cc
@@ -1,83 +1,91 @@
-#include <utility>
 #include <cmath>
+#include <fstream>
 #include <iostream>
 #include <set>
-#include <fstream>
+#include <utility>
 #include <vector>
 
-int dist(std::pair<int,int> p1, std::pair<int,int> p2) {
+int dist(std::pair<int, int> p1, std::pair<int, int> p2) {
   int x = std::abs(p1.first - p2.first);
   int y = std::abs(p1.second - p2.second);
-  return std::pow((x*x) + (y*y), 0.5);
+  return std::pow((x * x) + (y * y), 0.5);
 }
 
-std::pair<int,int> move(char dir, const std::pair<int,int>&  input) {
-  std::pair<int,int> H = input;
-  if (dir == 'U'){
+std::pair<int, int> move(char dir, const std::pair<int, int> &input) {
+  std::pair<int, int> H = input;
+  if (dir == 'U') {
     H.second++;
-  } else if (dir == 'D'){
+  } else if (dir == 'D') {
     H.second--;
-  } else if (dir == 'R'){
+  } else if (dir == 'R') {
     H.first++;
-  } else if (dir == 'L'){
+  } else if (dir == 'L') {
     H.first--;
   }
   return H;
 }
 
-int main() {
-  std::vector<std::pair<int,int>> rope {10};
-  std::vector<char> directions {'U','D','L','R'};
-  std::set<std::pair<int, int>> visit {};
-
-  std::fstream fs {"input_medium.txt"};
-  char dir;
-  int steps;
-  while (fs >> dir >> steps ){
-    for (int i = 0; i < steps; i++){
-      rope[0] = move(dir, rope[0]);
-      for (int i = 1; i < rope.size(); i++){
-        if (dist(rope[i-1], rope[i]) > 1){
-          std::pair<char, int> closestP = std::make_pair('U', 1000);
-          for (char potDir : directions){
-            int newDist = dist(move(potDir,rope[i-1]), rope[i]);
-            if (newDist < closestP.second){
-              closestP = std::make_pair(potDir,newDist);
-            }
-          }
-          //Use closestp to determine the what point to move rope tail.
-        }
-      }
-      visit.insert(rope.back());
-    }
+void moveDiag(std::pair<int, int> &rope, std::pair<int, int> &H) {
+  if (rope.first < H.first) {
+    rope = move('R', rope);
+  } else {
+    rope = move('L', rope);
   }
-  std::cout << visit.size();
-  return 0;
+  if (rope.second < H.second) {
+    rope = move('U', rope);
+  } else {
+    rope = move('D', rope);
+  }
 }
 
-  // std::pair<int,int> H = std::make_pair(0, 0);
-  // std::pair<int,int> T = std::make_pair(0, 0);
-  // std::set<std::pair<int, int>> visit {};
+bool moveStr(std::pair<int, int> &rope, std::pair<int, int> &H) {
+  bool res = false;
+  if (rope.first == H.first && rope.second < H.second) {
+    rope = move('U', rope);
+    res = true;
+  } else if (rope.first == H.first && rope.second >= H.second) {
+    rope = move('D', rope);
+    res = true;
+  } else if (rope.second == H.second && rope.first < H.first) {
+    rope = move('R', rope);
+    res = true;
+  } else if (rope.second == H.second && rope.first >= H.first) {
+    rope = move('L', rope);
+    res = true;
+  }
+  return res;
+}
 
-  // std::fstream fs {"input.txt"};
-  // char dir;
-  // int steps;
-  // while (fs >> dir >> steps ){
-  //   for (int i = 0; i < steps; i++){
-  //     H = move(dir,H);
-  //     if (dist(H, T) > 1){
-  //       if (dir == 'U'){
-  //         T = move('D', H);
-  //       } else if (dir == 'D'){
-  //         T = move('U', H);
-  //       } else if (dir == 'R'){
-  //         T = move('L', H);
-  //       } else if (dir == 'L'){
-  //         T = move('R', H);
-  //       }
-  //     }
-  //     visit.insert(T);
-  //   }
-  // }
-  // std::cout << visit.size();
-  // return 0;
+int main() {
+  std::vector<std::pair<int, int>> rope{10};
+  std::set<std::pair<int, int>> visitp1{};
+  std::set<std::pair<int, int>> visitp2{};
+
+  std::fstream fs{"input.txt"};
+  char dir;
+  int steps;
+  while (fs >> dir >> steps) {
+    for (int i = 0; i < steps; i++) {
+      // Move Head
+      rope[0] = move(dir, rope[0]);
+
+      // Move Body
+      for (int j = 1; j < rope.size(); j++) {
+        if (dist(rope[j - 1], rope[j]) > 1) {
+          bool moved = moveStr(rope[j], rope[j - 1]);
+          if (!moved) {
+            moveDiag(rope[j], rope[j - 1]);
+          }
+        }
+      }
+
+      // Print result
+      visitp2.insert(rope.back());
+      visitp1.insert(rope[1]);
+    }
+  }
+
+  std::cout << "Part 1: " << visitp1.size() << '\n';
+  std::cout << "Part 2: " << visitp2.size() << '\n';
+  return 0;
+}
